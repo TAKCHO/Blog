9becbac0ddd818dc5d17257cca7734cb216436b9
Tamari Giorgobiani <takogiorgobiani97@gmail.com>
2022-02-05 13:05:24 +0400

    add compose page

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+let posts = [];
+
 app.get("/", function(req,res){
   res.render("home", {startingContent: homeStartingContent});
+  console.log(posts);
 });
 
 app.get("/about", function(req,res){
@@ -27,9 +31,21 @@ app.get("/contact", function(req,res){
   res.render("contact", {contactContent1: contactContent});
 });
 
+app.get("/compose", function(req,res){
+  res.render("compose");
+});
 
+app.post("/compose", function(req,res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+ 
+ posts.push(post);
 
+ res.redirect("/");
 
+});
 
 
 
